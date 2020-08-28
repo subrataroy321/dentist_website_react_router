5d0d5ee1ae327974280559e2707b421988d8b5b7
subrataroy321 <subrata.r321@gmail.com>
2020-08-28 14:16:21 -0400

    [app] add links to nav

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import './App.css';
-import Home from './components/Home'
-import Procedures from './components/Procedures'
-import Contact from './components/Contact'
-
 import {
   BrowserRouter as Router,
   Route,
   Link 
 } from 'react-router-dom';
+import Home from './components/Home'
+import Procedures from './components/Procedures'
+import Contact from './components/Contact'
+
 
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <nav>
+          <Link to="/">Home</Link>{'  '}
+          <Link to="/procedures">Procedures</Link> {'  '}
+          <Link to="/contact">Contact Us!</Link>{'  '}
+        </nav>
         <Route exact path='/' component={Home}/>
         <Route path='/procedures' component={Procedures}/>
         <Route path='/contact' component={Contact}/>
