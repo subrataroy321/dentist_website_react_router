6f41400c669f493612c72c795fcb8945ac7b6acd
subrataroy321 <subrata.r321@gmail.com>
2020-08-28 14:00:44 -0400

    [procedures,contact] add procedures and contact component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Home from './components/Home'
+import Procedures from './components/Procedures'
+import Contact from './components/Contact'
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <Procedures />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Contact</h1>
+                <p>Subrata Roy, MD</p>
+                <p>email: subrata@dentist.com</p>
+                <p>Phone: 111-111-1111</p>
+            </div>
+        )
+    }
+}
+
+export default Contact;
